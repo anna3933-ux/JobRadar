cbf3a5ac5a400afe2235713e19d5ca451b5d0e61
Анна Скороходова <annaskorohodova3933@gmail.com>
2026-03-12 18:16:34 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { useEffect, useState } from "react";
 import { base44 } from "@/api/base44Client";
 import Layout from "@/components/Layout";
 import Dashboard from "@/pages/Dashboard";
-import VacanciesKanban from "@/pages/VacanciesKanban";
 import VacanciesList from "@/pages/VacanciesList";
 import VacancyDetail from "@/pages/VacancyDetail";
 import SearchSettings from "@/pages/SearchSettings";
